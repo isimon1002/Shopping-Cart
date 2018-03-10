d19723da074093ea3328d29424a01cd1dc5038a3
Isaac Simon <ijsimon@sbcglobal.net>
2018-03-10 21:52:59 +0000

    added navbar after completion refactor navbar for bootstrap 4.0.0

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,11 @@
   <meta charset="utf-8">
   <title>@yield('title')</title>
   @yield('styles')
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 </head>
 <body>
+@include('partials.header')
 @yield('content')
 <script
   src="https://code.jquery.com/jquery-1.12.4.min.js"
